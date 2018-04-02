168d892b14a5cab4df6d67e51d782e4a1d56b77f
whats0n <mrdvv@mail.ru>
2018-04-02 20:33:23 +0300

    update

--- src/js/app.js
+++ src/js/app.js
@@ -87,5 +87,29 @@ $(document).ready(function() {
     });
   }
 
+  {
+    $('.js-change-edit').on('click', function(e) {
+      const target = $(this);
+      const items = target.closest('.js-items').find('.js-item');
+      const parent = target.closest('.js-change');
+
+      items.addClass('is-removable');
+      parent.addClass('is-active');
+
+      e.preventDefault();
+    });
+
+    $('.js-edit-cancel, .js-edit-save').on('click', function(e) {
+      const target = $(this);
+      const items = target.closest('.js-items').find('.js-item');
+      const parent = target.closest('.js-change');
+
+      items.removeClass('is-removable');
+      parent.removeClass('is-active');
+
+      e.preventDefault();
+    });
+  }
+
 
 });
